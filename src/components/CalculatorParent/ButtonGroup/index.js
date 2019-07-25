0f336ed6554aa16f4a85e8fb7549104474753d8e
PKB2810 <pranay@kiprosh.com>
2019-07-25 17:31:34 +0530

    style change for button group

--- src/components/CalculatorParent/ButtonGroup/index.js
+++ src/components/CalculatorParent/ButtonGroup/index.js
@@ -1,19 +1,5 @@
 import React from "react";
 function ButtonGroup({ grpId, children }) {
-  return (
-    <section
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: "80%",
-        height: "16%",
-        alignItems: "center"
-      }}
-      id={grpId}
-    >
-      {children}
-    </section>
-  );
+  return <section id={grpId}>{children}</section>;
 }
 export default ButtonGroup;
